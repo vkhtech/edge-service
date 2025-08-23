6b83a0475168f21f1a576343bde3ea8cdf0ca273
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-08-23 20:52:53 +0200

    feat(security): implement basic authentication

--- src/main/java/org/vkhoma/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/org/vkhoma/edgeservice/config/RateLimiterConfig.java
@@ -3,14 +3,17 @@ package org.vkhoma.edgeservice.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 @Configuration
 public class RateLimiterConfig {
 
     @Bean
     public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 
 }
