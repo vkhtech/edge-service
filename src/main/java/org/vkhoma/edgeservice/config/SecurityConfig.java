8c845f1d8096505f0a57a4eb69cea6cae161c17a
vkhoma <16196348+HVV83@users.noreply.github.com>
2025-08-28 19:00:21 +0200

    feat: add health monitoring functionality

--- src/main/java/org/vkhoma/edgeservice/config/SecurityConfig.java
+++ src/main/java/org/vkhoma/edgeservice/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     ) {
         return http
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
                         .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
                         .anyExchange().authenticated()
